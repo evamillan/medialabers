1a8389576d8e58175bda138312d755566bd4b991
Blasapo <blasapo@gmail.com>
2017-05-30 19:23:39 +0200

    heeader modificado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var button = document.querySelector(".btn-mobile-size");
 var menuMobile = document.querySelector(".menu-mobile-screen");
 
-function toggle() {
+function toggleOpenClose() {
   if (menuMobile.classList.contains('hidden')) {
+      console.log("hola");
     menuMobile.classList.remove('hidden');
   } else {
     menuMobile.classList.add('hidden');
   }
 }
-button.addEventListener('click', toggle);
+button.addEventListener('click', toggleOpenClose);
