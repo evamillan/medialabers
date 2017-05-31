87174ea1cedbe1b0fbd156736333eb24b5a747e4
Cristina Fernández Rodríguez <cristinafernandezrodriguez@MacBook-Pro-de-Cristina-2.local>
2017-05-31 17:41:29 +0200

    resolve compile

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-<<<<<<< HEAD
+'use strict';
+
 var button = document.querySelector(".btn-mobile-size");
 var menuMobile = document.querySelector(".menu-mobile-screen");
 
 function toggleOpenClose() {
   if (menuMobile.classList.contains('hidden')) {
-      console.log("hola");
+    console.log("hola");
     menuMobile.classList.remove('hidden');
   } else {
     menuMobile.classList.add('hidden');
   }
 }
 button.addEventListener('click', toggleOpenClose);
-=======
-'user-strict'
-
+///////////////////
 $(document).ready(function() {
   $('#fullpage').fullpage();
   ({
     loopHorizontal: false,
   });
 });
->>>>>>> new structure
+//////////////new structure
