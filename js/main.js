d90b537342b0456984c89a2cf3dc0a69fec2586a
Eva Millán <evamillanmartin@gmail.com>
2017-05-31 18:04:42 +0200

    New navigation

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
-'use strict';
-
-var button = document.querySelector(".btn-mobile-size");
-var menuMobile = document.querySelector(".menu-mobile-screen");
-
-function toggleOpenClose() {
-  if (menuMobile.classList.contains('hidden')) {
-    console.log("hola");
-    menuMobile.classList.remove('hidden');
-  } else {
-    menuMobile.classList.add('hidden');
-  }
-}
-button.addEventListener('click', toggleOpenClose);
-///////////////////
 $(document).ready(function() {
-  $('#fullpage').fullpage();
-  ({
-    loopHorizontal: false,
+$('#fullpage').fullpage();({
+  loopHorizontal: false,
   });
 });
-//////////////new structure
