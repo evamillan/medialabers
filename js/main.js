766103d16b26baa51b384b77a2a59fdfb9df0bdb
Cristina Fernández Rodríguez <cristinafernandezrodriguez@MacBook-Pro-de-Cristina-2.local>
2017-06-01 10:24:03 +0200

    Revert "change-js-arrow"
    This reverts commit 70b988c32f349ed35a9b5fed1e902fed778e9486.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-'use strict';
-
 $(document).ready(function() {
-<<<<<<< HEAD
 	$('#fullpage').fullpage({
   controlArrows: false,
 });
-=======
-  $('#fullpage').fullpage();
-  ({
-    loopHorizontal: false,
-  });
->>>>>>> change-js-arrow
 });
-
-
-// function changeClaseCSS(){
-//   var elemento=document.getElementById(".fp-controlArrow.fp-next");
-//   if(existClase(elemento));
-//   {
-//     clearClass(elemento);
-//   };
-//   else {
-//     agregarClase(".new-fp-controlArrow.fp-next");
-//   }
-// };
